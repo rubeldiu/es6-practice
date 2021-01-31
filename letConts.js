5e55cf02bf93fe954e2399ae9588123735b94e73
hamiduldiu <hamiduldiu@gmail.com>
2021-01-31 20:30:01 +0100

    second commit

---
+++ letConts.js
@@ -0,0 +1,19 @@
+const hubby="Elias kanchon";
+//hubby="Omar Sunny";
+console.log(hubby);
+const numbers=[12,44];
+numbers[1]=88;
+numbers.push(13);
+//numbers=["Sunny","Salmab"]
+console.log(numbers);
+const nayok={name:"sakib khan",phone:456321};
+
+let patientName= "Rahim Chacha";
+patientName="Fatema Khala";
+console.log(patientName);
+
+let sum =0;
+for(let i=0; i<10;i++){
+    sum=sum+i;
+}
+console.log(sum);
---
+++ template.js
@@ -0,0 +1,9 @@
+const firstName="Justin";
+const lastName="Biver";
+//const fullNmae=firstName+lastName;
+const fullName=`Your fill name is ${firstName} ${lastName} and age is ${20+30+12}`;
+const multiLine=`I love you
+I miss You
+no. I don't need you`;
+console.log(multiLine)
+console.log(fullName);
